f4c98698f0a6c8f627599da8e8256495d8431b32
thaophamhetxat <thaophamhetxat@gmail.com>
2021-06-06 15:52:41 +0700

    hiển thị lời chào

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name!");
+        name = scanner.nextLine();
+        System.out.println("hello: " + name);
+    }
+}
